c0bc7d64c36f590e04099310586a9e1b2152552a
Soulberto Lorenzo <slorenzot@gmail.com>
2017-02-04 10:00:46 -0400

    - Added employee_hobby and employee_has_hobby tables - Added employee_has_hobby contraints - Added some initial data to employee_has_hobby table

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -19,9 +19,26 @@ CREATE TABLE employee_department (
 	CONSTRAINT pk_employee_department PRIMARY KEY(id)
 );
 
+
+CREATE TABLE employee_hobby (
+	id integer not null,
+	name character varying(30),
+	description character varying(90),
+	CONSTRAINT pk_employee_hobby PRIMARY KEY(id)
+);
+
+CREATE TABLE employee_has_hobby (
+	employee_id integer not null,
+	employee_hobby_id integer not null,
+	CONSTRAINT pk_employee_has_hobby PRIMARY KEY(employee_id, employee_hobby_id)
+);
+
+-- employee foreign key
 ALTER TABLE only employee add CONSTRAINT fk_employee_department_id FOREIGN KEY(department_id) REFERENCES public.employee_department(id);
 
-
+-- employee_hobby foreign keys
+ALTER TABLE only employee_has_hobby add CONSTRAINT fk_employee_has_hobby_employee_id FOREIGN KEY(employee_id) REFERENCES public.employee(id);
+ALTER TABLE only employee_has_hobby add CONSTRAINT fk_employee_has_hobby_hobby_id FOREIGN KEY(employee_hobby_id) REFERENCES public.employee_hobby(id);
 
 
 -- load some initial data
@@ -40,5 +57,20 @@ insert into employee(id, first_name, last_name, department_id) values(10002, 'jo
 insert into employee(id, first_name, last_name, department_id) values(10003, 'maria' , 'masa', 1200);
 insert into employee(id, first_name, last_name, department_id) values(10004, 'julio' , 'lopez', 2200);
 
+-- hobbies
+insert into employee_hobby(id, "name", description) values(1, 'video juegos' , 'video juegos');
+insert into employee_hobby(id, "name", description) values(2, 'musica' , 'escuchar musica e ir a conciertos');
+insert into employee_hobby(id, "name", description) values(3, 'peliculas' , 'ver peliculas e ir al cine');
+
+-- hobbies by employess
+insert into employee_has_hobby values(10001, 1);
+insert into employee_has_hobby values(10001, 3);
+insert into employee_has_hobby values(10002, 2);
+insert into employee_has_hobby values(10002, 3);
+insert into employee_has_hobby values(10003, 1);
+insert into employee_has_hobby values(10003, 2);
+insert into employee_has_hobby values(10003, 3);
+insert into employee_has_hobby values(10004, 2);
+insert into employee_has_hobby values(10004, 1);
 
 -- ...
